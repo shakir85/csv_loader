6375c1feafdc774816f79f2056813688cac01be8
shakir85 <salmasaari@hawk.iit.edu>
2019-08-17 00:12:21 -0500

    Re-arrange functions and make main()

--- csv_loader.py
+++ csv_loader.py
@@ -34,64 +34,68 @@ def unzip_file():
             fn = os.path.join(dir_path, file_)
             print("Extracted files:")
             print(fn)
-
-    # Join path + file name
-    # For later CSV file reading
+    # Join tmp folder path + file name
+    # For CSV file reading in DB function
     final_csv_file = os.path.join(tmp_dir, file_name)
-    return final_csv_file
-
-# Create table
-sql = "DROP TABLE IF EXISTS TRANSACTIONS"
-db_cursor.execute(sql)
-print("Table: TRANSACTIONS dropped successfully.\n")
-
-try:
-    sql = "CREATE TABLE TRANSACTIONS " \
-          "(ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " \
-          "STREET VARCHAR(245), " \
-          "CITY  VARCHAR(245), " \
-          "ZIP  VARCHAR(245), " \
-          "STATE CHAR(2), " \
-          "BEDS FLOAT, " \
-          "BATHS FLOAT, " \
-          "SQ_FT FLOAT, " \
-          "TYPE VARCHAR(245), " \
-          "SALE_DATE VARCHAR(245), " \
-          "PRICE DOUBLE, " \
-          "LATITUDE DOUBLE, " \
-          "LONGITUDE DOUBLE);"
+    return final_csv_file, tmp_dir
 
 
+# From the previous function,
+# pass in: unzipped csv file and temporary directory path
+def db_processing(csv_file_in, temporary_directory):
+    # Create table
+    sql = "DROP TABLE IF EXISTS TRANSACTIONS"
     db_cursor.execute(sql)
-    db_conn.commit()
-    print("Table EMP created successfully.\n")
+    print("Table: TRANSACTIONS dropped successfully.\n")
+    try:
+        sql = "CREATE TABLE TRANSACTIONS " \
+              "(ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " \
+              "STREET VARCHAR(245), " \
+              "CITY  VARCHAR(245), " \
+              "ZIP  VARCHAR(245), " \
+              "STATE CHAR(2), " \
+              "BEDS FLOAT, " \
+              "BATHS FLOAT, " \
+              "SQ_FT FLOAT, " \
+              "TYPE VARCHAR(245), " \
+              "SALE_DATE VARCHAR(245), " \
+              "PRICE DOUBLE, " \
+              "LATITUDE DOUBLE, " \
+              "LONGITUDE DOUBLE);"
+        db_cursor.execute(sql)
+        db_conn.commit()
+        print("Table EMP created successfully.\n")
 
-except mysql.connector.Error as err:
-    print("SQL Error in CREATE TABLE segment:\n", "{}".format(err), "\n")
+    except mysql.connector.Error as err:
+        print("SQL Error in CREATE TABLE segment:\n", "{}".format(err), "\n")
 
+    # Read unzipped csv file
+    with open(csv_file_in, newline='') as csv_file:
+        reader = csv.DictReader(csv_file)
+        # Insert to DB
+        for row in reader:
+            try:
+                sql = "INSERT INTO TRANSACTIONS (STREET, CITY, ZIP, STATE, BEDS, BATHS, SQ_FT, TYPE, SALE_DATE, PRICE, LATITUDE, LONGITUDE) " \
+                      "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                val = (row['street'], row['city'], row['zip'], row['state'],row['beds'], row['baths'], row['sq__ft'], row['type'],row['sale_date'], row['price'], row['latitude'], row['longitude'])
 
-# Assign to csv reader
-with open(final_csv_file, newline='') as csv_file:
-    reader = csv.DictReader(csv_file)
-    # Insert to DB
-    for row in reader:
-        try:
-            sql = "INSERT INTO TRANSACTIONS (STREET, CITY, ZIP, STATE, BEDS, BATHS, SQ_FT, TYPE, SALE_DATE, PRICE, LATITUDE, LONGITUDE) " \
-                  "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-            val = (row['street'], row['city'], row['zip'], row['state'],row['beds'], row['baths'], row['sq__ft'], row['type'],row['sale_date'], row['price'], row['latitude'], row['longitude'])
+                db_cursor.execute(sql, val)
+                db_conn.commit()
+                print(db_cursor.rowcount, " record inserted.\r")
 
-            db_cursor.execute(sql, val)
-            db_conn.commit()
-            print(db_cursor.rowcount, " record inserted.\r")
+            except mysql.connector.Error as err:
+                print("SQL Error in INSERT segment:\n", "{}".format(err), "\n")
+    #Close DB Connection
+    db_conn.close
+    db_cursor.close()
 
-        except mysql.connector.Error as err:
-            print("SQL Error in INSERT segment:\n", "{}".format(err), "\n")
+    # Delete $HOME/tmp
+    shutil.rmtree(temporary_directory)
+    print("Directory\t", temporary_directory, "\tdeleted successfully")
 
-#Close DB Connection
-db_conn.close
-db_cursor.close()
+def main():
+    final_csv_file, tmp_dir = unzip_file()
+    db_processing(final_csv_file, tmp_dir)
 
-# Delete $HOME/tmp
-shutil.rmtree(tmp_dir)
-print("Directory\t", tmp_dir, "\tdeleted successfully")
-
+if __name__ == '__main__':
+    main()
