1c39e7ac7174036892ee446199abb79a863f9a28
shakir85 <sralmasaari@gmail.com>
2020-01-28 23:59:38 -0600

    Remove template script

--- csv_loader.py
+++ csv_loader.py
@@ -1,3 +1,6 @@
+"""
+Python script for zip file extract & load into MySQL table
+"""
 import csv
 import shutil
 import sys
@@ -9,7 +12,7 @@ import mysql.connector
 db_conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="root", # not a pswd
+    passwd="root",  # not a pswd
     database="dataloader"
 )
 db_cursor = db_conn.cursor()
@@ -42,6 +45,8 @@ def unzip_file():
 
 # From the previous function,
 # pass in: unzipped csv file and temporary directory path
+
+
 def db_processing(csv_file_in, temporary_directory):
     # Create table
     sql = "DROP TABLE IF EXISTS TRANSACTIONS"
@@ -74,19 +79,20 @@ def db_processing(csv_file_in, temporary_directory):
         reader = csv.DictReader(csv_file)
         print('Processing . . .')
 
-        #Insert into DB
+        # Insert into DB
         try:
             for row in reader:
                 sql = "INSERT INTO TRANSACTIONS (STREET, CITY, ZIP, STATE, BEDS, BATHS, SQ_FT, TYPE, SALE_DATE, PRICE, LATITUDE, LONGITUDE) " \
                       "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-                val = (row['street'], row['city'], row['zip'], row['state'],row['beds'], row['baths'], row['sq__ft'], row['type'],row['sale_date'], row['price'], row['latitude'], row['longitude'])
+                val = (row['street'], row['city'], row['zip'], row['state'], row['beds'], row['baths'],
+                       row['sq__ft'], row['type'], row['sale_date'], row['price'], row['latitude'], row['longitude'])
                 db_cursor.execute(sql, val)
 
                 sys.stdout.write('Processing \r    ')
                 sys.stdout.flush()
 
         except mysql.connector.Error as err:
-                print("SQL Error in INSERT segment:\n", "{}".format(err), "\n")
+            print("SQL Error in INSERT segment:\n", "{}".format(err), "\n")
     # Close DB Connection
     db_conn.commit()
     db_conn.close
@@ -98,9 +104,11 @@ def db_processing(csv_file_in, temporary_directory):
     shutil.rmtree(temporary_directory)
     print("Temporary directory:\t", temporary_directory, "\t- deleted successfully")
 
+
 def main():
     final_csv_file, tmp_dir = unzip_file()
     db_processing(final_csv_file, tmp_dir)
+
 
 if __name__ == '__main__':
     main()
